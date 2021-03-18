46580ff6fa25b6c4751ff1c482b59ef5133608cf
mack-a <jerrymattmeng@gmail.com>
2021-03-18 17:31:18 +0800

    feat(update): update

--- client/app.js
+++ client/app.js
@@ -10,8 +10,8 @@ var App = React.createClass({
         <span className="app_title">Hexo Admin</span>
         <ul className="app_nav">
           <li><Link to="posts">Posts</Link></li>
-          {/*<li><Link to="pages">Pages</Link></li>*/}
-          {/*<li><Link to="about">About</Link></li>*/}
+          <li><Link to="pages">Pages</Link></li>
+          <li><Link to="about">About</Link></li>
           <li><Link to="deploy">Deploy</Link></li>
           <li><Link to="settings">Settings</Link></li>
         </ul>
